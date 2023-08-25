01ba2f549d962a5bf269e3a3e49cd9d21abd03a1
N0oCh1 <110440573+N0oCh1@users.noreply.github.com>
2023-08-25 17:47:39 -0500

    Pain

--- js/juego.js
+++ js/juego.js
@@ -1,11 +1,101 @@
+var nick;
+var avatar
 
-getDatosUsuario();
-if(comprobarDatosUsuario()==false) 
-{
-    location = "index.html";
+function domLoaded() {
+    nick = document.getElementById('usuario');
+    avatar = document.getElementById('avatar');
+    nick.value = sessionStorage.getItem('name');
+    avatar.style.backgroundColor = sessionStorage.getItem('avatarColor');
+    console.log(avatar.style.backgroundColor);
+    if(comprobarDatosUsuario()==false) 
+    {
+        console.log(comprobarDatosUsuario());
+        location = "index.html";
+
+    }
 }
-console.log(comprobarDatosUsuario());
+document.addEventListener('DOMContentLoaded', domLoaded);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//codigo opcional
+const showInfo = document.getElementById('Info');
+const dropHistory = document.getElementById('borrarHistorico');
+const infoBox = document.getElementById('location');
+//geolocalizacion API funtion
+
+function geolocalizacion (position){
+    const latitud = position.coords.latitude;
+    const longitud = position.coords.longitude;
+
+    infoBox.textContent = `latitiud: ${(latitud)} , longitud: ${(longitud)}`;
+}
+navigator.geolocation.getCurrentPosition(geolocalizacion);
+getDatosUsuario();
+
+console.log(comprobarDatosUsuario());
+if(localStorage.getItem('historico') == null){
+    showInfo.innerText = "No hay historico";
+}
+else{
+    showInfo.innerText = localStorage.getItem('historico');
+}
+
+dropHistory.addEventListener('click', ()=>{
+    localStorage.removeItem('historico');
+    location.reload();
+});
+//fin del codigo opcional
 
 
 
--- js/userStorage.js
+++ js/userStorage.js
@@ -1,15 +1,22 @@
 
 var nick;
+var localizacion;
+
+
 
 function datosUsuarios (name){
     sessionStorage.setItem('name', name.value);
+    getDatosUsuario();
 }
 function getDatosUsuario(){
     nick = sessionStorage.getItem('name');
-    console.log(nick);
+    
 }
-
+function avatarColor(color) {
+    sessionStorage.setItem('avatarColor', color);
+}
 function comprobarDatosUsuario(){
+    console.log(nick);
     if(nick == null)
     {
         sessionStorage.setItem('error', "rellene el formulario");
@@ -20,4 +27,21 @@ function comprobarDatosUsuario(){
         return true;
     }
 }
+
+function guardarHistoricos(nick) {
+    let historicoStorage = localStorage.getItem('historico');
+    let historico;
     
+    if(historicoStorage==null){
+        historico = [];
+    }
+    else{
+        historico = JSON.parse(historicoStorage);
+    }
+    let registro = {
+        userName : nick.value,
+        date : Date.now()
+    }
+    historico.push(registro);
+    localStorage.setItem('historico', JSON.stringify(historico));
+}
